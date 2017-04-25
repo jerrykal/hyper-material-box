e9c7c8b507ef5c3b000b4cae66a904e68e3b52da
Jerry Lee <oojerryleeoo@gmail.com>
2017-04-25 21:35:41 +0800

    :sparkles: Add solarized dark and light color scheme

---
+++ scheme/solarized_dark.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  colors: {
+    black: '#073642',
+    red: '#DC322F',
+    green: '#859900',
+    yellow: '#B58900',
+    blue: '#268BD2',
+    magenta: '#D33682',
+    cyan: '#2AA198',
+    white: '#EEE8D5',
+    lightBlack: '#596E75',
+    lightRed: '#DC322F',
+    lightGreen: '#859900',
+    lightYellow: '#B58900',
+    lightBlue: '#268BD2',
+    lightMagenta: '#D33682',
+    lightCyan: '#2AA198',
+    lightWhite: '#FDF6E3',
+  },
+
+  // Default
+  backgroundColor: '#002B36',
+  foregroundColor: '#839490',
+  cursorColor: '#EEE8D5',
+  borderColor: 'transparent',
+
+  // Accent color
+  accentColor: '#268BD2',
+
+  // Other
+  inactiveTabTitleColor: 'rgba(255, 255, 255, 0.2)',
+  activeTabTitleColor: '#DDD',
+};
---
+++ scheme/solarized_light.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  colors: {
+    black: '#073642',
+    red: '#DC322F',
+    green: '#859900',
+    yellow: '#B58900',
+    blue: '#268BD2',
+    magenta: '#D33682',
+    cyan: '#2AA198',
+    white: '#EEE8D5',
+    lightBlack: '#93A1A1',
+    lightRed: '#DC322F',
+    lightGreen: '#859900',
+    lightYellow: '#B58900',
+    lightBlue: '#268BD2',
+    lightMagenta: '#D33682',
+    lightCyan: '#2AA198',
+    lightWhite: '#FDF6E3',
+  },
+
+  // Default
+  backgroundColor: '#FDF6E3',
+  foregroundColor: '#657B83',
+  cursorColor: '#596E75',
+  borderColor: 'transparent',
+
+  // Accent color
+  accentColor: '#2AA198',
+
+  // Other
+  inactiveTabTitleColor: 'rgba(0, 0, 0, 0.3)',
+  activeTabTitleColor: '#000',
+};
