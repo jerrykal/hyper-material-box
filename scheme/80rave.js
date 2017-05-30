065d2786461588a14be93dc8b6159491eb816c39
Petr Barborka <grim.ramone@gmail.com>
2017-05-30 14:35:47 +0200

    Initial commit for the 80rave theme :tada: :lipstick:

---
+++ scheme/80rave.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  colors: {                                                                                                                            
+      black: '#2f002f',                                                                                                                    
+      red: '#EF00FF',                                                                                                                      
+      green: '#FF6A00',                                                                                                                    
+      yellow: '#FF6A00',                                                                                                                   
+      blue: '#00EFFF',                                                                                                                     
+      magenta: '#EF00FF',                                                                                                                  
+      cyan: '#00EFFF',                                                                                                                     
+      white: '#FFFFFF',                                                                                                                    
+      lightBlack: '#3f003f',                                                                                                               
+      lightRed: '#EF00FF',                                                                                                                 
+      lightGreen: '#00EFFF',                                                                                                               
+      lightYellow: '#FF6A00',                                                                                                              
+      lightBlue: '#00EFFF',                                                                                                                
+      lightMagenta: '#EF00FF',                                                                                                             
+      lightCyan: '#00EFFF',                                                                                                                
+      lightWhite: '#FFFFFF',                                                                                                               
+  },                                                                                                                                   
+
+  activeTabStyle : 'underline',                                                                                                        
+  highlightActiveTab : 'true',                                                                                                         
+  accentColor: '#EF00FF',                                                                                                              
+  lightEffect: 'true',                                                                                                                 
+
+  // Default                                                                                                                           
+  backgroundColor: '#3a003a',                                                                                                          
+  foregroundColor: '#DDFFFF',                                                                                                          
+  cursorColor: '#FF6A00',                                                                                                              
+  borderColor: 'transparent',    
+};
