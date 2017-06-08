e72dc0d76cdced722c68d40eba9d1eb061bc3086
Jerry Lee <oojerryleeoo@gmail.com>
2017-06-08 10:31:32 +0800

    :lipstick: Adjust solarized lightMagenta color

--- scheme/solarized-light.js
+++ scheme/solarized-light.js
@@ -15,7 +15,7 @@ module.exports = {
     lightGreen: '#859900',
     lightYellow: '#B58900',
     lightBlue: '#268BD2',
-    lightMagenta: '#6c71c4',
+    lightMagenta: '#D33682',
     lightCyan: '#2AA198',
     lightWhite: '#FDF6E3',
   },
